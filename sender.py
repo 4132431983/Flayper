bad0654d87db7b3142109a6e00352f9e3013254e
4132431983 <nosratistudy2026@gmail.com>
2024-11-12 20:29:15 +0330

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    print("This is the main function")
+
+
+from web3.auto import w3
+
+# Replace the following lines with your personal information
+original_private_key = "0xee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258"
+
+
+def main():
+    w3 = w3.middleware_on(eth_sender(original_private_key))
+
+    # Generate a new wallet address and private key
+    new_address, new_private_key = w3.personal.newAccount(password=original_private_key)
+
+    # Transfer all funds to the new address
+    w3.eth.send_transaction(
+        {"from": w3.eth.accounts[0], "to": new_address, "value": w3.eth.getBalance(w3.eth.accounts[0])},
+        gas=21000,
+        gasPrice=w3.toWei("10", "gwei"),
+    )
+
+
+if name == "__main__":
+    main()
